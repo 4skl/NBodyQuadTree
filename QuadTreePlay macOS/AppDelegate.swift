8044f7a1838999e129323366ee7d6f0de6192abf
4skl <medi.olivier@hotmail.fr>
2021-07-26 11:43:43 +0200

    Initial Commit

---
+++ QuadTreePlay macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  QuadTreePlay macOS
+//
+//  Created by Medi Olivier on 26/07/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
